8480fcc810cf145c7074adef212e761cfdaa694d
gaki2745 <guyeongjun@naver.com>
2019-11-13 20:25:58 +0900

    [프로젝트생성] 대략적인구성

---
+++ ARNavigation/Controller/MainViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ARNavigation
+//
+//  Created by youngjun goo on 2019/11/13.
+//  Copyright © 2019 youngjun goo. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
