23e4ee6dad8c07d822c5832bc6cd95c8787626c5
gaki <guyeongjun@naver.com>
2019-12-27 12:08:20 +0900

    Naver Driving API, Naver Search API  Reqeust 작업, RxSwift 적용 완료

--- ARNavigation/App/Common/Constants.swift
+++ ARNavigation/App/Common/Constants.swift
@@ -8,13 +8,22 @@
 
 import Foundation
 
-struct NaverAPI {
+struct NaverSearchAPI {
+    static let scheme: String = "https"
+    static let host: String = "openapi.naver.com"
+    static let path = "/v1/search/local.json"
     // Naver Search
     static let NSLClientId: String = "NSLClientId"
     static let NSLClientSecret: String = "NSLClientSecret"
     static let NSLbaseURL: String = "https://openapi.naver.com/v1/search/local.json"
     static let NSLClientIdHeader: String = "X-Naver-Client-Id"
     static let NSLClientSecretHeader: String = "X-Naver-Client-Secret"
+}
+
+struct NaverDirectionAPI {
+    static let scheme: String = "https"
+    static let host: String = "naveropenapi.apigw.ntruss.com"
+    static let path: String = "map-direction/v1/driving"
     // Naver Driving
     static let NMFClientId: String = "NMFClientId"
     static let NMFClientSecret: String = "NMFClientSecret"
---
+++ ARNavigation/App/Presentation/NMView/NMViewModel.swift
@@ -0,0 +1,23 @@
+//
+//  NMViewModel.swift
+//  ARNavigation
+//
+//  Created by youngjun goo on 2019/12/24.
+//  Copyright © 2019 youngjun goo. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+import RxCocoa
+import NMapsMap
+
+//class NMViewModel {
+//    
+//    // OutPut
+//    var directionPoints: Signal<[NMGLatLng]>
+//    var nmMarker: Signal<[NMFMarker]>
+//    var nmPath: Signal<NMFPath>
+//    
+//    private let apiService: APIService
+//    
+//}
